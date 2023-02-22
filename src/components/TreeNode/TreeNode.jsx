eb9590b63cadbe2b42425436196d7215990fed20
Dave Ekhaus <david_ekhaus@apple.com>
2023-02-22 11:28:00 -0800

    more debug

--- src/components/TreeNode/TreeNode.jsx
+++ src/components/TreeNode/TreeNode.jsx
@@ -114,6 +114,7 @@ const TreeNode = ({
 
   const handleNameClick = (event) => {
     console.log('DSE::handleNameClick.event', event)
+    console.log('DSE::handleNameClick.nodeData', nodeData)
     const defaultOnClick = selectMe;
     if (onNameClick && typeof onNameClick === 'function') {
       !isEditing && onNameClick({ defaultOnClick, nodeData });
