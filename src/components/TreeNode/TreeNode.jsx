d26beae018c953472eb978f91ad60f775f727234
Dave Ekhaus <david_ekhaus@apple.com>
2023-03-28 12:12:26 -0700

    added icon/color classes

--- src/components/TreeNode/TreeNode.jsx
+++ src/components/TreeNode/TreeNode.jsx
@@ -205,7 +205,7 @@ const TreeNode = ({
 
         { isFolder && folderCaret }
 
-        <span className={ iconContainerClassName('typeIconContainer') }>
+        <span className={ iconContainerClassName('typeIconContainer') + `${ isDark ? ' react-folder-tree-text-icon-dark' : ' react-folder-tree-text-icon-light' }`}>
           <TypeIcon
             className={ iconClassName(TypeIconType) }
             onClick={ selectMe }
